e4128d531aba6c5f1d090589a23e8af7ad2a4c1c
Ceyvion Biggs <ceyvionb@gmail.com>
2019-11-30 00:52:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 def #introduction introduction(name)
-  puts "Hi, my name is #{name}."
+  puts "\n Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
